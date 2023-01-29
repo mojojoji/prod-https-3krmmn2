498a75ead3fe5cc17f76b14b1251f768d0ca7d68
CodeSandbox <support@codesandbox.io>
2023-01-29 23:10:30 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+import _ from "lodash";
+
+export default function App() {
+  console.log(_);
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox v2!</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
